6d0a6ebd47e742dbbf91fa98c9400ed884e8fa72
StupachenkoArtem <119942880+StupachenkoArtem@users.noreply.github.com>
2023-09-25 17:29:13 +0300

    commit

--- recordbook/students/viewsets.py
+++ recordbook/students/viewsets.py
@@ -6,7 +6,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Student, Group
-from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
+from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly, UserPermission
 from .serializers import StudentSerializer, StudentDetailSerializer
 from .utils import StudentAPIPagination
 
@@ -56,7 +56,7 @@ class GroupAPIView(APIView):
 
 class StudentViewSet(viewsets.ModelViewSet):
     pagination_class = StudentAPIPagination
-    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly, )
+    permission_classes = (UserPermission, )
     # queryset = Student.objects.all()
     # serializer_class = StudentSerializer
 
