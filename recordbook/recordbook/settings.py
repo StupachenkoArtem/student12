b4b132e4bfc67d30b1736b4691c6b82388485815
StupachenkoArtem <119942880+StupachenkoArtem@users.noreply.github.com>
2023-09-28 17:00:26 +0300

    commit

--- recordbook/recordbook/settings.py
+++ recordbook/recordbook/settings.py
@@ -44,6 +44,14 @@ INSTALLED_APPS = [
     'djoser',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
